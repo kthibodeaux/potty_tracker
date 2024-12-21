49fb6acabb7fb3e4fe961344c1d8acf552af7da8
Keith Thibodeaux <keith@railscoder.net>
2024-12-21 09:09:30 -0500

    simplify

--- main.rb
+++ main.rb
@@ -6,25 +6,10 @@ MQTT_HOST = '10.1.20.1'
 MQTT_PORT = 8005
 MQTT_TOPIC = 'potty-tracker'
 
-def log(action)
-  File.open('data/potty-tracker.log', 'a') do |f|
-    f.puts "#{Time.now},#{action}"
-  end
-end
-
 MQTT::Client.connect(MQTT_HOST, MQTT_PORT) do |c|
   c.get(MQTT_TOPIC) do |_, message|
-    case message
-    when 'accident'
-      log :accident
-    when 'poop'
-      log :poop
-    when 'pee'
-      log :pee
-    when 'poop and pee'
-      log :poop and pee
-    when 'attempt'
-      log :attempt
+    File.open('data/potty-tracker.log', 'a') do |f|
+      f.puts "#{Time.now},#{message}"
     end
   end
 end
