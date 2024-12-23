9f0ad5304758bd0b0535e2ee6e12e1c05b27432c
Keith Thibodeaux <keith@railscoder.net>
2024-12-23 15:15:15 -0500

    log messages to postgres instead of file

--- mqtt-client.rb
+++ mqtt-client.rb
@@ -1,15 +1,16 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'mqtt'
+require 'sequel'
 
+DB = Sequel.connect ENV['DATABASE_URL']
 MQTT_HOST = '10.1.20.1'
 MQTT_PORT = 8005
 MQTT_TOPIC = 'potty-tracker'
 
 MQTT::Client.connect(MQTT_HOST, MQTT_PORT) do |c|
   c.get(MQTT_TOPIC) do |_, message|
-    File.open('data/potty-tracker.log', 'a') do |f|
-      f.puts "#{Time.now},#{message}"
-    end
+    DB[:events].insert(event_type: message, timestamp: Time.now)
   end
 end
