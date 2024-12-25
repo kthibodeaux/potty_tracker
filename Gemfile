b08293dccb3b9ec578cdb51ce80de4f49376ddff
Keith Thibodeaux <keith@railscoder.net>
2024-12-25 08:57:12 -0500

    index lists current days logs

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@
 
 source 'https://rubygems.org'
 
+gem 'activesupport'
 gem 'mqtt'
 gem 'pg'
 gem 'puma'
--- web.rb
+++ web.rb
@@ -1,14 +1,23 @@
 # frozen_string_literal: true
 
+require 'active_support/time'
+require 'json'
 require 'sinatra'
 require 'sequel'
-require 'json'
 
 DB = Sequel.connect ENV['DATABASE_URL']
+TIMEZONE = 'America/New_York'
 
 set :bind, '0.0.0.0'
 
 get '/' do
+  raw_date = params[:date] ? Date.parse(params[:date]) : Date.today
+  @date = raw_date.in_time_zone(TIMEZONE)
+
+  utc_start = @date.beginning_of_day.getlocal('+00:00')
+  utc_end = @date.end_of_day.getlocal('+00:00')
+
+  @events = DB[:events].order(:timestamp).where(timestamp: utc_start..utc_end).all
   slim :index
 end
 
